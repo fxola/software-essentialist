b019c27df719f2020d48fe180d8cc80723c0c0c2
fxola <afolabiabass62@gmmail.com>
2023-05-15 23:36:00 +0100

    [refactor]: add extra test examples for numbers out of the 1-100 range

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -4,32 +4,32 @@ describe("fizzbuzz", () => {
   const mulitplesOfThree = [3, 9, 42];
   const mulitplesOfFive = [5, 20, 50];
   const mulitplesOfThreeAndFive = [15, 45, 90];
+  const numbersOutOfBound = [-20, -12, 101, 102];
 
   it("returns a string when provided with a valid number", () => {
     expect(typeof fizzbuzz(60)).toBe("string");
   });
 
-  it("returns an empty string when provided with an invalid number", () => {
-    expect(fizzbuzz(-20)).toBe("");
-  });
-
-  it("returns an empty string when provided with an invalid number", () => {
-    expect(fizzbuzz(101)).toBe("");
-  });
+  it.each(numbersOutOfBound)(
+    "returns an empty string when provided with an invalid number like %s",
+    (value) => {
+      expect(fizzbuzz(value)).toBe("");
+    }
+  );
 
   it("returns an empty string when provided with a non number", () => {
     expect(fizzbuzz(Number("smooth"))).toBe("");
   });
 
   it.each(mulitplesOfThree)(
-    `returns "Fizz" when provided with multiples of 3`,
+    `returns "Fizz" when provided with multiples of 3 like %s`,
     (value) => {
       expect(fizzbuzz(value)).toBe("Fizz");
     }
   );
 
   it.each(mulitplesOfFive)(
-    `returns "Buzz" when provided with multiples of 5`,
+    `returns "Buzz" when provided with multiples of 5 like %s`,
     (value) => {
       expect(fizzbuzz(value)).toBe("Buzz");
     }
