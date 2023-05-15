fa420cbb45c3246e87eaa19fc42468bf1b85471b
fxola <afolabiabass62@gmmail.com>
2023-05-15 22:54:38 +0100

    [green]: return Fizz when called with multiples of 3

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,7 @@
 export const fizzbuzz = (value: number): string => {
   if (isNaN(value) || value < 1 || value > 100) return "";
+
+  if (value % 3 === 0) return "Fizz";
+
   return value.toString();
 };
