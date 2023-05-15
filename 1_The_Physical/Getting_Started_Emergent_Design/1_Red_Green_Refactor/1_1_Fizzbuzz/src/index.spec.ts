500d727a1e602c0e6fbdf610b4e3978428602838
fxola <afolabiabass62@gmmail.com>
2023-05-15 22:11:06 +0100

    [red]: get fizzbuzz to fail for an acceptable reason

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,7 @@
+import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
-
-
+  it("returns a string when provided with a valid number", () => {
+    expect(typeof fizzbuzz(60)).toBe("string");
+  });
 });
