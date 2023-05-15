6881da942405cada40affde8a136b3cc819110ce
fxola <afolabiabass62@gmmail.com>
2023-05-15 22:46:45 +0100

    [green]: add check for non number input to code

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,4 +1,4 @@
 export const fizzbuzz = (value: number): string => {
-  if (value < 1 || value > 100) return "";
+  if (isNaN(value) || value < 1 || value > 100) return "";
   return value.toString();
 };
