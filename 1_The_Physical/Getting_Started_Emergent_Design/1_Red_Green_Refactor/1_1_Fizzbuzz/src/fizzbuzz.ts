a7e89dd12a7bb1985ea0df18b9f7ead0c715f112
fxola <afolabiabass62@gmmail.com>
2023-05-15 23:06:25 +0100

    [refactor]: abstract logic for checking number mulitples into functions

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,8 +1,11 @@
+const isMultipleOfThree = (value: number) => value % 3 === 0;
+const isMultipleOfFive = (value: number) => value % 5 === 0;
+
 export const fizzbuzz = (value: number): string => {
   if (isNaN(value) || value < 1 || value > 100) return "";
 
-  if (value % 3 === 0) return "Fizz";
-  if (value % 5 === 0) return "Buzz";
+  if (isMultipleOfThree(value)) return "Fizz";
+  if (isMultipleOfFive(value)) return "Buzz";
 
   return value.toString();
 };
