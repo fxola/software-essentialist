9a98f57739383d8f2c5999776fd2f598f48aa31f
fxola <afolabiabass62@gmmail.com>
2023-05-15 22:26:21 +0100

    [red]: add new test case to screen invalid input

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -4,4 +4,8 @@ describe("fizzbuzz", () => {
   it("returns a string when provided with a valid number", () => {
     expect(typeof fizzbuzz(60)).toBe("string");
   });
+
+  it("returns an empty string when provided with an invalid number", () => {
+    expect(fizzbuzz(-20)).toBe("");
+  });
 });
