061bb2e57564d8f4b8e7ed2b1209b5bfc16b5f18
fxola <afolabiabass62@gmmail.com>
2023-05-15 23:12:30 +0100

    [green]: return FizzBuzz when called with a multiple of 3 and 5

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -4,6 +4,7 @@ const isMultipleOfFive = (value: number) => value % 5 === 0;
 export const fizzbuzz = (value: number): string => {
   if (isNaN(value) || value < 1 || value > 100) return "";
 
+  if (isMultipleOfThree(value) && isMultipleOfFive(value)) return "FizzBuzz";
   if (isMultipleOfThree(value)) return "Fizz";
   if (isMultipleOfFive(value)) return "Buzz";
 
