8727139c87d51172f98d1b83ac82bcb7f87bd328
fxola <afolabiabass62@gmmail.com>
2023-05-15 22:59:56 +0100

    [green]: return Buzz when called with multiples of 5

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -2,6 +2,7 @@ export const fizzbuzz = (value: number): string => {
   if (isNaN(value) || value < 1 || value > 100) return "";
 
   if (value % 3 === 0) return "Fizz";
+  if (value % 5 === 0) return "Buzz";
 
   return value.toString();
 };
