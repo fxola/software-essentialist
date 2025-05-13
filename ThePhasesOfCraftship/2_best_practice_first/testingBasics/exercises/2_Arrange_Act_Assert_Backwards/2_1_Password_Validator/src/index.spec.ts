d144c3d73dd721b1582d3233552c0528f0ac3ffd
fxola <afolabiabass62@gmail.com>
2025-05-13 11:15:50 +0100

    green: knows that password not meeting any criteria returns multiple errors

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -16,6 +16,7 @@ describe("password validator", () => {
     expect(output.errors.length).toBeGreaterThanOrEqual(1);
     expect(output.errors).toContain(errorTypes.invalidPasswordLength);
   });
+
   it("knows that password greater than 15 characters is invalid", () => {
     const output = PasswordValidator.validate("sm1th3r33nsFromGam3OfThron3s");
 
@@ -23,11 +24,13 @@ describe("password validator", () => {
     expect(output.errors.length).toBeGreaterThanOrEqual(1);
     expect(output.errors).toContain(errorTypes.invalidPasswordLength);
   });
+
   it("knows that password between 5 and 15 is valid", () => {
     const output = PasswordValidator.validate("manGr00ve");
     expect(output.result).toBeTruthy();
     expect(output.errors.length).toBe(0);
   });
+
   it("knows that password containing no digit is invalid", () => {
     const output = PasswordValidator.validate("smithereens");
 
@@ -35,12 +38,14 @@ describe("password validator", () => {
     expect(output.errors.length).toBeGreaterThanOrEqual(1);
     expect(output.errors).toContain(errorTypes.noDigitsInPassword);
   });
+
   it("knows that password containing atleast 1 digit is valid", () => {
     const output = PasswordValidator.validate("Smithereens20");
 
     expect(output.result).toBeTruthy();
     expect(output.errors.length).toBe(0);
   });
+
   it("knows that password containing no uppercase letter is invalid", () => {
     const output = PasswordValidator.validate("smithereens20");
 
@@ -48,10 +53,21 @@ describe("password validator", () => {
     expect(output.errors.length).toBeGreaterThanOrEqual(1);
     expect(output.errors).toContain(errorTypes.noUppercaseLetterInPassword);
   });
+
   it("knows that password containing atleast 1 uppercase letter is valid", () => {
     const output = PasswordValidator.validate("Valid01passworD");
 
     expect(output.result).toBeTruthy();
     expect(output.errors.length).toBe(0);
+  });
+
+  it("knows that password not meeting all the required criterias returns muiltiple errors", () => {
+    const output = PasswordValidator.validate("tiny");
+
+    expect(output.result).toBeFalsy();
+    expect(output.errors.length).toBe(3);
+    expect(output.errors).toContain(errorTypes.invalidPasswordLength);
+    expect(output.errors).toContain(errorTypes.noDigitsInPassword);
+    expect(output.errors).toContain(errorTypes.noUppercaseLetterInPassword);
   });
 });
