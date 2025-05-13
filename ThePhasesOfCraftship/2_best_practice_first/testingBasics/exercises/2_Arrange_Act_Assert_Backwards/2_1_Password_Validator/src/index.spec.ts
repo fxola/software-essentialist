eaa93d26a2f6009df0d700e96d05c2d8965a5d15
fxola <afolabiabass62@gmail.com>
2025-05-13 08:02:58 +0100

    green: knows that password less than 5 characters in invalid

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -1,9 +1,25 @@
+import PasswordValidator, { errorTypes } from "./index";
 
-describe('password validator', () => {
+describe("password validator", () => {
+  //Between 5 and 15 characters long
+  // Contains at least one digit
+  // Contains at least one upper case letter
+  // Return an object containing a boolean result
+  // and an errors key that — when provided with an invalid password
+  // — contains an error message or type for all errors in occurrence.
+  // There can be multiple errors at a single time.
 
-  test('hello', () => {
-    expect("between 5 and 15").toContain('5 and 15')
-  })
-})
+  it("knows that password less than 5 characters is invalid", () => {
+    const output = PasswordValidator.validate("s3rv");
 
-
+    expect(output.result).toBeFalsy();
+    expect(output.errors.length).toBeGreaterThanOrEqual(1);
+    expect(output.errors).toContain(errorTypes.invalidPasswordLength);
+  });
+  it.todo("knows that password greater than 15 characters is invalid");
+  it.todo("knows that password between 5 and 15 is valid");
+  it.todo("knows that password containing no digit is invalid");
+  it.todo("knows that password containing atleast 1 digit is valid");
+  it.todo("knows that password containing no uppercase letter is invalid");
+  it.todo("knows that password containing atleast 1 uppercase letter is valid");
+});
---
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.ts
@@ -0,0 +1,36 @@
+//Write a function (or a stateless class) for validating passwords.
+// Passwords must meet the following criteria:
+//Between 5 and 15 characters long
+// Contains at least one digit
+// Contains at least one upper case letter
+// Return an object containing a boolean result
+// and an errors key that — when provided with an invalid password
+// — contains an error message or type for all errors in occurrence.
+// There can be multiple errors at a single time.
+
+type ValidationResult = {
+  result: boolean;
+  errors: ErrorMessages[];
+};
+
+export const errorTypes = {
+  invalidPasswordLength: "Invalid password length. Must be between 5 and 15",
+} as const;
+
+type ErrorMessages = (typeof errorTypes)[keyof typeof errorTypes];
+
+export default class PasswordValidator {
+  public static validate(password: string): ValidationResult {
+    let errors: ErrorMessages[] = [];
+
+    const isValidPasswordLength = password.length >= 5;
+    if (!isValidPasswordLength) {
+      errors.push(errorTypes.invalidPasswordLength);
+    }
+
+    return {
+      result: errors.length < 1,
+      errors,
+    };
+  }
+}
