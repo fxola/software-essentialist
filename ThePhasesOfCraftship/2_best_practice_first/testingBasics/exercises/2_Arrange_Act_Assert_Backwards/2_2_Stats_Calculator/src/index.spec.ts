603f3ba0f88acfc9596d50ea6a40d40106ef8c9a
fxola <afolabiabass62@gmail.com>
2025-05-18 10:12:21 +0100

    new: list out all responsibilities

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/2_Arrange_Act_Assert_Backwards/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,5 +1,22 @@
+//Description: Your task is to process a sequence of integer numbers to determine the following statistics:
+// Without using system Math library functions, process a sequence of integers
+// to determine the following statistics:
+//minimum value = -8,
+//  maximum value = 53,
+// number of elements in the sequence = 6,
+// average value = 18.666666666667
 
-describe('stats calculator', () => {
+//doings -
+// compute minimum value
+// compute max value
+// compute number of elements
+// compute average value
 
+describe("stats calculator", () => {
+  it.todo("knows that 1 is the minimum value in [1,2,3,4]");
+  it.todo("knows that 4 is the minimum value in [1,2,3,4]");
 
-})
+  it.todo("knows that 4 is the number of elements in [1,2,3,4]");
+
+  it.todo("knows that 2.5 is the average value in [1,2,3,4]");
+});
