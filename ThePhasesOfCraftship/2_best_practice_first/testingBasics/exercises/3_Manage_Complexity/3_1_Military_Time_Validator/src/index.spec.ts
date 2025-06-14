b3d2bf9419ce6d0a3e381cc9e61950a3e6931fe2
fxola <afolabiabass62@gmail.com>
2025-06-14 11:36:34 +0100

    new:Knows that time has maximum of 23 hours and a maximum of 59 minutes

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -9,5 +9,19 @@ describe("military time validator", () => {
     it('knows that "23:00 - 23:45" is valid', () => {
       expect(MilitaryTime.validate("23:00 - 23:45")).toBeTruthy();
     });
+
+    it('knows that "23:00 - 23:01" is valid', () => {
+      expect(MilitaryTime.validate("23:00 - 23:01")).toBeTruthy();
+    });
+
+    it('knows that "13:00 - 09:01" is invalid', () => {
+      expect(MilitaryTime.validate("13:00 - 09:01")).toBeFalsy();
+    });
+  });
+
+  describe("Knows that time has maximum of 23 hours and a maximum of 59 minutes", () => {
+    it('knows that "25:00 - 26:01" is invalid', () => {
+      expect(MilitaryTime.validate("25:00 - 26:01")).toBeFalsy();
+    });
   });
 });
--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,9 +1,16 @@
 export class MilitaryTime {
   public static validate(timeString: string) {
     const [startTime, endTime] = timeString.split(" - ");
-    const [startTimeHour, startTimeMinute] = startTime.split(":");
-    const [endTimeHour, endTimeMinute] = endTime.split(":");
+    const [startTimeHour, startTimeMinute] = startTime.split(":").map(Number);
+    const [endTimeHour, endTimeMinute] = endTime.split(":").map(Number);
 
-    return startTimeHour <= endTimeHour;
+    if (startTimeHour > endTimeHour) return false;
+
+    const isValidTimeRange = (hr: number, min: number) => hr <= 23 && min <= 59;
+
+    return (
+      isValidTimeRange(startTimeHour, startTimeMinute) &&
+      isValidTimeRange(endTimeHour, endTimeMinute)
+    );
   }
 }
