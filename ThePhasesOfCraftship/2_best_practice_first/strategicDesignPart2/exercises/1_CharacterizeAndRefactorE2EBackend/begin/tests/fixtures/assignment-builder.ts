7d0f222d49806356ebd2cbd102c653f031e377fa
fxola <afolabiabass62@gmail.com>
2025-07-05 12:06:50 +0100

    chore: create classroom and assignment builders

---
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/tests/fixtures/assignment-builder.ts
@@ -0,0 +1,43 @@
+import { Assignment } from "@prisma/client";
+import { prisma } from "../../src/database";
+import { ClassroomBuilder } from "./classroom-builder";
+
+export class AssignmentBuilder {
+  private classroomBuilder: ClassroomBuilder;
+  private assignment: Partial<Assignment>;
+
+  constructor() {
+    this.assignment = {};
+  }
+
+  withTitle(title: string) {
+    this.assignment.title = title;
+    return this;
+  }
+
+  withClassRoom(classRoomBuilder: ClassroomBuilder) {
+    this.classroomBuilder = classRoomBuilder;
+    return this;
+  }
+
+  async build() {
+    if (!this.classroomBuilder) {
+      throw new Error("A class must first be created");
+    }
+
+    if (!this.assignment.title) {
+      throw new Error("Title must be provided");
+    }
+
+    const classroom = await this.classroomBuilder.build();
+
+    const assignment = await prisma.assignment.create({
+      data: {
+        classId: classroom.id,
+        title: this.assignment.title,
+      },
+    });
+
+    return assignment;
+  }
+}
---
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/tests/fixtures/classroom-builder.ts
@@ -0,0 +1,29 @@
+import { Class } from "@prisma/client";
+import { prisma } from "../../src/database";
+
+export class ClassroomBuilder {
+  private classroom: Partial<Class>;
+
+  constructor() {
+    this.classroom = {};
+  }
+
+  withName(name: string) {
+    this.classroom.name = name;
+    return this;
+  }
+
+  async build() {
+    const name = this.classroom.name;
+
+    if (!name) {
+      throw new Error("You need a name to create a classroom");
+    }
+
+    return await prisma.class.create({
+      data: {
+        name,
+      },
+    });
+  }
+}
